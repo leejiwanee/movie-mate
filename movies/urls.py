7e0d0cc04d7309864e26bb08af75693177f9ddf5
Jiwan Lee <78126718+leejiwanee@users.noreply.github.com>
2026-06-13 22:48:16 -0400

    Implement v2.0: Curation and Recommendation Engine logic

--- movies/urls.py
+++ movies/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('search/', views.search_movies, name='search'),
     path('watchlist/', views.get_watchlist, name='watchlist'),
     path('watchlist/add/<int:movie_id>/', views.add_to_watchlist, name='add_to_watchlist'),
+    path('tonight/', views.what_should_i_watch, name='tonight'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,11 @@
 from pymongo import MongoClient
-from django.http import JsonResponse, render
+from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render
+from django.conf import settings
 
+# TMDB API 기본 주소 설정
+BASE_URL = 'https://api.themoviedb.org/3'
 client = MongoClient('mongodb://localhost:27017/')
 db = client['moviemate_db']
 watchlist_collection = db['watchlist']
@@ -17,3 +21,9 @@ def add_to_watchlist(request, movie_id):
 def get_watchlist(request):
     movies = list(watchlist_collection.find({}, {'_id': 0}))
     return render(request, 'movies/watchlist.html', {'movies': movies})
+
+def what_should_i_watch(request):
+    url = f"{BASE_URL}/discover/movie?api_key={settings.TMDB_API_KEY}&sort_by=popularity.desc&vote_average.gte=7.5"
+    response = requests.get(url)
+    recommendations = response.json().get('results', [])[:5] 
+    return render(request, 'movies/tonight.html', {'movies': recommendations})
